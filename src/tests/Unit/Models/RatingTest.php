52dbed068f837228b3f078379a04fb240d0eba81
Tanat <researcher2286@gmail.com>
2018-11-25 13:27:39 +0600

    Refactoring RatingTest

--- src/tests/Unit/Models/RatingTest.php
+++ src/tests/Unit/Models/RatingTest.php
@@ -11,38 +11,41 @@ class RatingTest extends TestCase
     /**
      * @dataProvider providerInvalidData
      */
-    public function testCreateInvalidRating()
+    public function testCreateInvalidRating($number)
     {
-        foreach (func_get_args() as $number) {
-            $this->expectException(\DomainException::class);
-            $this->expectExceptionMessage(sprintf('Rating is not included in the range %d..%d', Rating::MIN, Rating::MAX));
-            Rating::create($number, factory(User::class)->create()->id);
-        }
+        $this->expectException(\DomainException::class);
+        $this->expectExceptionMessage(sprintf('Rating is not included in the range %d..%d', Rating::MIN, Rating::MAX));
+        Rating::create($number, factory(User::class)->create()->id);
     }
 
     /**
      * @dataProvider providerValidData
      */
-    public function testCreateValidRating()
+    public function testCreateValidRating($number)
     {
-        foreach (func_get_args() as $number) {
-            $rating = Rating::create($number, factory(User::class)->create()->id);
-            $this->assertNotNull($rating);
-            $this->assertNotNull($rating->user);
-        }
+        $rating = Rating::create($number, factory(User::class)->create()->id);
+        $this->assertNotNull($rating);
+        $this->assertNotNull($rating->user);
     }
 
     public function providerInvalidData()
     {
         return [
-            [-1, 0, 6, 7],
+            ' -1' => [-1],
+            ' 0' => [0],
+            ' 6' => [6],
+            ' 7' => [7],
         ];
     }
 
     public function providerValidData()
     {
-        return [
-            [1, 2, 3, 4, 5],
-        ];
+        return array(
+            ' 1' => [1],
+            ' 2' => [2],
+            ' 3' => [3],
+            ' 4' => [4],
+            ' 5' => [5],
+        );
     }
 }
