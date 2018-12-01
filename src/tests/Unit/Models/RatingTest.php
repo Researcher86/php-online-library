227c077ad5d009a908ea073ef06f93a91ed402a7
Tanat <researcher2286@gmail.com>
2018-12-01 12:33:55 +0600

    Update RatingTest

--- src/tests/Unit/Models/RatingTest.php
+++ src/tests/Unit/Models/RatingTest.php
@@ -31,21 +31,21 @@ class RatingTest extends TestCase
     public function providerInvalidData()
     {
         return [
-            ' -1' => [-1],
-            ' 0' => [0],
-            ' 6' => [6],
-            ' 7' => [7],
+            [-1],
+            [0],
+            [6],
+            [7],
         ];
     }
 
     public function providerValidData()
     {
         return array(
-            ' 1' => [1],
-            ' 2' => [2],
-            ' 3' => [3],
-            ' 4' => [4],
-            ' 5' => [5],
+            [1],
+            [2],
+            [3],
+            [4],
+            [5],
         );
     }
 }
