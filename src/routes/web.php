54ccaed58fbf487c97f5abcd56fa50c97c5b2fd4
Tanat <researcher2286@gmail.com>
2019-01-27 11:10:10 +0600

    Упростить рендеринг для главной страницы

--- src/routes/web.php
+++ src/routes/web.php
@@ -16,4 +16,6 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::get('/', function () {
+    return view('home');
+});
--- src/tests/Feature/HomePageTest.php
+++ src/tests/Feature/HomePageTest.php
@@ -5,7 +5,7 @@ namespace Tests\Feature;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-class HomeControllerTest extends TestCase
+class HomePageTest extends TestCase
 {
     /**
      * A basic test example.
