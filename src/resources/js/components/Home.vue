0d64c18ed6b469bb92e2c76d1529baf6b5236965
Tanat <researcher2286@gmail.com>
2019-02-09 17:25:25 +0600

    Добавить индикатор загрузки

--- src/resources/js/components/Home.vue
+++ src/resources/js/components/Home.vue
@@ -1,6 +1,13 @@
 <template>
     <div class="row justify-content-center">
-        <div class="col-md-3">
+        <div class="col-mod-12" v-bind:class="[!loading ? 'd-none' : '']">
+            <div class="text-center">
+                <div class="spinner-border" role="status">
+                    <span class="sr-only">Loading...</span>
+                </div>
+            </div>
+        </div>
+        <div class="col-md-3" v-bind:class="[loading ? 'd-none' : '']">
             <div class="card">
                 <div class="card-header">Жанры</div>
 
@@ -12,7 +19,7 @@
                 </div>
             </div>
         </div>
-        <div class="col-md-9">
+        <div class="col-md-9" v-bind:class="[loading ? 'd-none' : '']">
             <books :genre="genre"></books>
         </div>
     </div>
@@ -26,6 +33,7 @@
             return {
                 genres: [],
                 genre: null,
+                loading: true
             }
         },
         created() {
@@ -38,6 +46,7 @@
                     .then(res => res.json())
                     .then(res => {
                         this.genres = res;
+                        this.loading = false;
                     })
                     .catch(err => console.log(err));
             },
