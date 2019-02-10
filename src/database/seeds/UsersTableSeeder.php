3e4f8e762085bb508862a9bb9db92d44756284e3
Tanat <researcher2286@gmail.com>
2019-02-10 17:28:57 +0600

    Добавить создание дополнительных пользователей в UsersTableSeeder

--- src/database/seeds/UsersTableSeeder.php
+++ src/database/seeds/UsersTableSeeder.php
@@ -8,6 +8,15 @@ class UsersTableSeeder extends Seeder
 {
     public function run()
     {
-        User::create(['last_name'=>'Альпенов', 'first_name' => 'Танат', 'middle_name' => 'Маратович', 'email' => 'researcher2286@gmail.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test2@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test3@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test4@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test5@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test6@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test7@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test8@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test9@test.com', 'password' => Hash::make('123')]);
+        factory(User::class)->create(['email' => 'test10@test.com', 'password' => Hash::make('123')]);
     }
 }
