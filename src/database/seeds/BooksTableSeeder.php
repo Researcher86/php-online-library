02b18f284bf20fd16f58f980827bc202750fe9cf
Tanat <researcher2286@gmail.com>
2019-04-28 12:03:25 +0600

    Refactoring BooksTableSeeder

--- src/database/seeds/BooksTableSeeder.php
+++ src/database/seeds/BooksTableSeeder.php
@@ -1,14 +1,15 @@
 <?php
 
-use App\Handler\Book\BookParser;
-use App\Handler\Book\ClearDir;
-use App\Handler\Book\FindFile;
-use App\Handler\Book\Indexer;
-use App\Handler\Book\MoveImage;
-use App\Handler\Book\Printer;
+use App\Models\Book\Author;
+use App\Models\Book\Book;
+use App\Models\Book\Genre;
+use App\Models\Book\Image;
+use App\Models\Book\Rating;
+use App\Models\User;
 use App\Services\Book\Index\IndexBookServiceInterface;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Log;
 
 class BooksTableSeeder extends Seeder
 {
@@ -28,13 +29,162 @@ class BooksTableSeeder extends Seeder
 
     public function run()
     {
-        $root = new ClearDir(__DIR__ . '/../../storage/app/public/files/');
-        $root->setNext(new FindFile(__DIR__ . '/books/*/*/*.json'))
-            ->setNext(new MoveImage(__DIR__ . '/../../storage/app/public/files/'))
+        $filesPath = __DIR__ . '/../../storage/app/public/files/';
+        if (PHP_OS === 'Windows' && file_exists($filesPath)) {
+            exec(sprintf("rd /s /q %s", escapeshellarg($filesPath)));
+        } else {
+            exec(sprintf("rm -rf %s", escapeshellarg($filesPath)));
+        }
+
+        $root = new Printer();
+        $root->setNext(new MoveImage($filesPath))
             ->setNext(new BookParser())
-//            ->setNext(new Printer())
             ->setNext(new Indexer($this->indexBookService));
 
-        $root->handle(null);
+        $iterator = new RecursiveIteratorIterator(new RecursiveDirectoryIterator(__DIR__ . '/books'));
+        $results = new RegexIterator($iterator, '/^.+\.json/i', RecursiveRegexIterator::GET_MATCH);
+
+        foreach ($results as $file) {
+            $root->handle($file[0]);
+        }
+
+    }
+}
+
+class Printer extends AbstractHandler
+{
+    public function handle($request)
+    {
+        Log::info($request);
+        parent::handle($request);
+    }
+}
+
+class MoveImage extends AbstractHandler
+{
+    private $destDir;
+
+    /**
+     * MoveFile constructor.
+     * @param $destDir
+     */
+    public function __construct($destDir)
+    {
+        $this->destDir = $destDir;
+    }
+
+    public function handle($jsonFile)
+    {
+        $bookId = microtime();
+        $json = json_decode(file_get_contents($jsonFile));
+        $imageName = basename($json[0]);
+
+        $sourceDir = dirname($jsonFile) . '/' . $imageName;
+        $destDir = $this->destDir . date('Y-m-d') . '/books/' . $bookId;
+        if (!file_exists($destDir)) {
+            mkdir($destDir, 0777, true);
+        }
+        try {
+            copy($sourceDir, $destDir . '/' . $imageName);
+
+            parent::handle(['json_file' => $jsonFile, 'image' => '/files/' . str_replace($this->destDir, '', $destDir) . '/' . $imageName]);
+        } catch (\Exception $e) {
+            Log::warning($this->destDir);
+            Log::warning($destDir);
+            Log::error($e->getMessage());
+        }
+    }
+
+}
+
+class BookParser extends AbstractHandler
+{
+    private $user;
+
+    /**
+     * BookParser constructor.
+     */
+    public function __construct()
+    {
+        $this->user = User::take(1)->first();
+    }
+
+    public function handle($request)
+    {
+        $json = json_decode(file_get_contents($request['json_file']));
+
+        $jsonFile = preg_replace('/.*\/books\//', '', $request['json_file']);
+
+        preg_match('/(.*?)\/(.*?)\//', $jsonFile, $matches);
+        $genreName = $this->replaceSymbols($matches[1]);
+        $bookName = $this->replaceSymbols($matches[2]);
+        $authorName = $this->replaceSymbols($json[2]);
+        $annotation = $this->replaceSymbols($json[3]);
+
+        if (!Book::where('title', '=', $bookName)->first()) {
+            $genre = Genre::firstOrCreate(['name' => $genreName]);
+            $author = Author::firstOrCreate(['name' => $authorName]);
+            $image = Image::firstOrCreate(['file' => $request['image']]);
+
+            /** @var Book $book */
+            $book = Book::create(['title' => $bookName, 'annotation' => $annotation]);
+            $book->addGenre($genre);
+            $book->addAuthor($author);
+            $book->addImage($image);
+            $book->addRating(Rating::create(random_int(1, 5), $this->user->id));
+
+            parent::handle($book);
+        }
+    }
+
+    private function replaceSymbols(string $string)
+    {
+        return preg_replace('/«|»/s', '"', $string);
+    }
+}
+
+class Indexer extends AbstractHandler
+{
+    /**
+     * @var IndexBookServiceInterface
+     */
+    private $indexBookService;
+
+    /**
+     * Indexer constructor.
+     * @param IndexBookServiceInterface $indexBookService
+     */
+    public function __construct(IndexBookServiceInterface $indexBookService)
+    {
+        $this->indexBookService = $indexBookService;
+    }
+
+    public function handle($request)
+    {
+        $this->indexBookService->add($request);
+        parent::handle($request);
+    }
+
+}
+
+
+abstract class AbstractHandler
+{
+    /**
+     * @var AbstractHandler
+     */
+    private $nextHandler;
+
+    public function setNext(AbstractHandler $handler): AbstractHandler
+    {
+        $this->nextHandler = $handler;
+        return $handler;
+    }
+
+    public function handle($request)
+    {
+        if ($this->nextHandler) {
+            $this->nextHandler->handle($request);
+        }
     }
 }
